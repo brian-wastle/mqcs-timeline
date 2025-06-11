e90ed528806bbbbfa9a056a3775aecfdb88b9f26
Brian Wastle <brian.wastle1@gmail.com>
2025-06-11 17:04:23 -0400

    ngx-lightbox for homepage images

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { ImageLightboxComponent } from '../image-lightbox/image-lightbox.component';
+import { CommonModule } from '@angular/common';
+import { LightboxModule } from 'ngx-lightbox';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-homepage',
-  imports: [ImageLightboxComponent],
+  imports: [CommonModule, LightboxModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  public albums: Array<any> = [];
+  constructor(private lightbox: Lightbox) {
+    
+      const src = 'assets/images/solarsystemmap.webp';
+      const caption = '';
+      const thumb = 'assets/images/solarsystemmap.webp';
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+      this.albums.push(album);
+  }
+
+  open(index: number): void {
+    // open lightbox
+    this.lightbox.open(this.albums, index, {
+      centerVertically: true,
+      resizeDuration: 0.001,
+      fitImageInViewPort: true,
+      showZoom: true
+    });
+  }
+
+  close(): void {
+    // close lightbox programmatically
+    this.lightbox.close();
+  }
+
 
 }

baa87033c32f67ec57cc86ac20bce738d3b304ef
Brian Wastle <brian.wastle1@gmail.com>
2025-06-24 16:18:12 -0400

    responsive css for timeline component, title added

--- src/app/directives/on-hover/on-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnHoverDirective } from './on-hover.directive';
-
-describe('OnHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
